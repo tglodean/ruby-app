5a8da5c13fd9ad3d1a03982322672d7db215c843
Tudor Glodean <tglodean@tgn-desktop>
2014-05-21 13:18:10 +0300

    Exercises chapter 7

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -128,6 +128,12 @@ describe "User pages" do
     let(:submit) { "Create my account" }
 
     describe "with invalid information" do
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+      end
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
